9f4d2426dc8c68f6794d8d7dfaf690e6a8f15d35
Brian Wangora <brianwangora@gmail.com>
2022-06-07 22:04:20 +0300

    wrapAdjective not passed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun (activity) {
+    activity = (typeof activity !== 'undefined') ? activity : "roller-skate"
+    return `This Saturday, I want to ${activity}!`
+}
+
+// define function using a function expression.
+// If nothing is passed in, default to "go to the office"
+const mondayWork = function (activity) {
+    activity = (typeof activity !== 'undefined') ? activity : "go to the office"
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective (flair) {
+    const innerFunction = function (adjective) {
+        adjective = (typeof adjective !== 'undefined') ? adjective : "special"
+        return `You are ${adjective}!`; 
+    };
+    flair = (typeof flair !== 'undefined') ? flair: "*"
+    
+    return 
+}
